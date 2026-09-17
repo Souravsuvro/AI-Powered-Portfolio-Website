f94f31235b50355294db3e2cd0d8c1d03a31312b
Sourav Sarker Suvra <58484707+Souravsuvro@users.noreply.github.com>
2026-09-17 22:39:28 +0100

    Add package.json, Vite, Vercel, index.html, and TypeScript configs

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (
+              id.includes('recharts') ||
+              id.includes('react-syntax-highlighter') ||
+              id.includes('unsplash-js')
+            ) {
+              return 'vendor-charts';
+            }
+            if (
+              id.includes('framer-motion') ||
+              id.includes('react-type-animation')
+            ) {
+              return 'vendor-animations';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+});
